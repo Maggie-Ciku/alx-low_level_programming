0144e74c1570f2f4dd3536a0cfffa53b90e11717
Margaret Wanjiku <margaret.wanjiku0883@gmail.com>
2022-06-02 15:38:23 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,12 +4,12 @@
 /**
  * main - Starting point
  *
- * Return: return value 1
+ * Return: return the value 1
  *
  */
 
 int main(void)
 {
-	write(2, "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n", 59);
+	write(2, "\"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
