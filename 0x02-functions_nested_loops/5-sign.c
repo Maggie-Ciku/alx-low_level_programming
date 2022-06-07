62da9b71722f0550bf2cb85a3a80de506a6c53ee
Margaret Wanjiku <margaret.wanjiku0883@gmail.com>
2022-06-07 16:22:25 -0700

    task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * int print_sign - Starting point
+ * print_sign - Starting point
  *  @n: The character in ASCII code
  *  Description:  prints the sign of a number.
  *  Return: 1 (greater than zero) , 0 (is zero) or -1 (less than zero)
  */
-int print_sign(int n);
+int print_sign(int n)
 {
 	if (n > 0)
 	{
