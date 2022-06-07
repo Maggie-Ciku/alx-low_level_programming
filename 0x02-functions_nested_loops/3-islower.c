da26f2f256be05fc10ad6b42044f026ca73c353b
Margaret Wanjiku <margaret.wanjiku0883@gmail.com>
2022-06-07 14:36:18 -0700

    task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _islower - Starting point
+ *
+ * Description: Checks for lowercase character.
+ *
+ * Return: 1 (Lower case) . 1 (Otherwise)
+ */
+int _islower(void)
+{
+	int c;
+
+	if (c >= a && c <= z; c++)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n')
+}
