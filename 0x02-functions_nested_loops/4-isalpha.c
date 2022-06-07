3c557b14d4765999dedfdb8a7c152f5a02af079c
Margaret Wanjiku <margaret.wanjiku0883@gmail.com>
2022-06-07 15:33:38 -0700

    task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isalpha - Starting point
+ *  @c: The character in ASCII code
+ *  Description: checks for alphabetic character
+ *  Return: 1 if c is a letter, lowercase or uppercase
+ */
+int _isalpha(int c)
+{
+	if ((c >= 97 & c <= 122) || (c >= 65 && c <= 90))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
