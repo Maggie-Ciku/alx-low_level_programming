245c2aabd9630e823c2db100bdfd007b1513a0b9
Margaret Wanjiku <margaret.wanjiku0883@gmail.com>
2022-06-07 17:17:46 -0700

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -4,8 +4,29 @@
  *
  * Return: Always 0.
  */
-int main(void)
+void times_table(void);
 {
-	times_table();
-	return (0);
+	int a, b, c;
+
+	for (a = 0; a <= 9; a++)
+	{
+		_putchar(48);
+		for (b = 1; b <= 9; b++)
+		{
+			c = a * b;
+			_putchar(44);
+			_putchar(32);
+			if (c <= 9)
+			{
+				_putchar(32);
+				_putchar(c + 48);
+			}
+			else
+			{
+				_putchar((c / 10) + 48);
+				_putchar((c % 10) + 48);
+			}
+		}
+		_putchar('\n');
+	}
 }
