a4d7b84e463a2c141da4970e0538cd35a5bed35d
Margaret Wanjiku <margaret.wanjiku0883@gmail.com>
2022-06-09 11:44:06 -0700

    task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _isupper - Starting point
+ * @c: alphabet
+ * Description: checks for uppercase character.
+ * Return: Always 0
+ */
+int _isupper(int c);
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+-m 0x04-more_functions_nested_loops readme file
