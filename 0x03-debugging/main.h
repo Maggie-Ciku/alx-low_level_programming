e23c6116e74f36fcf44528a9a00b2eb1f9a73b3d
Margaret Wanjiku <margaret.wanjiku0883@gmail.com>
2022-06-09 11:15:09 -0700

    task 3

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,3 +1,4 @@
 void positive_or_negativeniint();
 int largest_number();
-void print_remaining_days();
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
