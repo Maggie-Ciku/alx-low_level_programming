f78e8dd91d47f4eeff5400436c802aa355778d07
Margaret Wanjiku <margaret.wanjiku0883@gmail.com>
2022-06-09 12:34:26 -0700

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * mul - Starting point
+ * Description: multiplies two integers.
+ * @a: int a
+ * @b: int b
+ * Return: the product
+ */
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+
+	return (c);
+}
