a566db18344baa6928cbca3fe5477fd96abc24be
Margaret Wanjiku <margaret.wanjiku0883@gmail.com>
2022-06-20 12:51:21 -0700

    pointers, arrays and strings

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * *_strchr -  Starting point
+ * Description:  locates a character in a string.
+ * @s: the string
+ * @c: the character
+ * Return: a pointer to a character
+ */
+char *_strchr(char *s, char c)
+{
+	int a;
+
+	for (a = 0; s[a] != '\0' ; a++)
+	{
+		if (s[a] == c)
+			return (&s[a]);
+	}
+	return (0);
+}
